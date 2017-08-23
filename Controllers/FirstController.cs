90e922fdbeca8fc504a0f5bd0d926d9aae977f73
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-23 10:52:05 -0400

    first done

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WelcomeToTheWeb.Models;
+
+namespace WelcomeToTheWeb.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
